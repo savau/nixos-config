2525ad380fb6280a84e54f32b7722d43258c8d07
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-07 15:34:52 +0000

    tweak zsh.nix

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -3,23 +3,10 @@
 {
   programs.zsh = {
     enable = true;
-    #interactiveShellInit = ''
-    #  export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
-    #
-    #  ZSH_THEME="agnoster"
-    #  plugins=(git)
-
-    #  source $ZSH/oh-my-zsh.sh
-    #'';
-    #promptInit = "";  # avoid conflicts with oh-my-zsh
     ohMyZsh = {
       enable = true;
+      theme = "agnoster";
       plugins = [ "git" ];
     };
   };
-
-  environment.systemPackages = with pkgs; [
-    oh-my-zsh
-    powerline
-  ];
 }
