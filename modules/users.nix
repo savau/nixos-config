582ba8e7fd71e1061332a9e911dc1b5e8b138d00
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-09 15:43:49 +0100

    set root shell to ZSH

--- modules/users.nix
+++ modules/users.nix
@@ -4,19 +4,23 @@
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = true;
 
-  users.users.savau = {
-    description = "Sarah Vaupel";
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "audio" "video"
-      "systemd-journal"
-      "networkmanager"
-      "vboxusers"
-      "libvirtd"
-      "docker"
-    ];
-    shell = pkgs.zsh;
-    uid = 1000;
+  users = {
+    extraUsers.root.shell = pkgs.zsh;
+    
+    users.savau = {
+      description = "Sarah Vaupel";
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "audio" "video"
+        "systemd-journal"
+        "networkmanager"
+        "vboxusers"
+        "libvirtd"
+        "docker"
+      ];
+      shell = pkgs.zsh;
+      uid = 1000;
+    };
   };
 }
