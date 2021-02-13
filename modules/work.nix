84ba7c125a4392d53451f82fa4e1d236e69cd2c2
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-13 15:48:16 +0100

    refactor work

--- modules/work.nix
+++ modules/work.nix
@@ -5,52 +5,52 @@
     ./work/direnv.nix
     ./work/u2w.nix
   ];
+
+  boot.tmpOnTmpfs = true;
   
+  programs = {
+    gnupg.agent.enable = true;
+    ssh.startAgent = true;
+  };
+
+  services = {
+    printing.enable = true;
+  };
+  
+  virtualisation = {
+    docker.enable = true;
+    libvirtd.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     # build-essentials
     binutils
     gcc
     gnumake
+    gup
+    nodejs
     pkgconfig
     python
-    nodejs
     stack
     texlive.combined.scheme-full
-    gup
 
     # prerequisites for Haskell Language Server
-    ncurses
     icu.dev
+    ncurses
     zlib
     
     # utilities
-    wget
     unzip
+    wget
 
     # nix
     nix-prefetch-git
     
     # desktop applications
-    texstudio
-    rstudio
-    libreoffice
-    zoom
     jetbrains.idea-community
+    libreoffice
+    rstudio
+    texstudio
+    zoom
   ];
-
-  virtualisation = {
-    libvirtd.enable = true;
-    docker.enable = true;
-  };
-
-  boot.tmpOnTmpfs = true;
-
-  services = {
-    printing.enable = true;
-  };
-
-  programs = {
-    ssh.startAgent = true;
-    gnupg.agent.enable = true;
-  };
 }
