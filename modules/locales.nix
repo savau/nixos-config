8f2929b342927d53585718be2b4afe44e9b12929
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-13 14:22:32 +0100

    switch to fewer supported locales

--- modules/locales.nix
+++ modules/locales.nix
@@ -8,6 +8,12 @@
 
   i18n = {
     defaultLocale = "en_DK.UTF-8";
+    supportedLocales = [
+      "en_DK.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "en_GB.UTF-8/UTF-8"
+      "de_DE.UTF-8/UTF-8"
+    ];
   };
 
   environment.systemPackages = with pkgs; [
