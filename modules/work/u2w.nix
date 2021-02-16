ef9853ca85d4da1f41c7a8573b6f76650c4923c1
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-16 16:29:31 +0100

    disable u2w dependencies for now

--- modules/work/u2w.nix
+++ modules/work/u2w.nix
@@ -3,39 +3,39 @@
 {
   services = {
     # TODO: do I need this?
-    openldap = {
-      enable = true;
-    };
+    #openldap = {
+    #  enable = true;
+    #};
 
-    postgresql = {
-      enable = true;
-      authentication = lib.mkForce ''
-        # TYPE  DATABASE  USER  ADDRESS       METHOD
-        local   all       all                 trust
-        host    all       all   127.0.0.1/32  trust
-        host    all       all   ::1/128       trust
-      '';
-      identMap = ''
-        uniworx uniworx uniworx
-      '';
-      ensureDatabases = [
-        "uniworx"
-        "uniworx_test"
-      ];
-      ensureUsers = [
-        {
-          name = "uniworx";
-          ensurePermissions = {
-            "DATABASE uniworx" = "ALL PRIVILEGES";
-            "DATABASE uniworx_test" = "ALL PRIVILEGES";
-          };
-        }
-      ];
-    };
+    #postgresql = {
+    #  enable = true;
+    #  authentication = lib.mkForce ''
+    #    # TYPE  DATABASE  USER  ADDRESS       METHOD
+    #    local   all       all                 trust
+    #    host    all       all   127.0.0.1/32  trust
+    #    host    all       all   ::1/128       trust
+    #  '';
+    #  identMap = ''
+    #    uniworx uniworx uniworx
+    #  '';
+    #  ensureDatabases = [
+    #    "uniworx"
+    #    "uniworx_test"
+    #  ];
+    #  ensureUsers = [
+    #    {
+    #      name = "uniworx";
+    #      ensurePermissions = {
+    #        "DATABASE uniworx" = "ALL PRIVILEGES";
+    #        "DATABASE uniworx_test" = "ALL PRIVILEGES";
+    #      };
+    #    }
+    #  ];
+    #};
   };
 
-  environment.systemPackages = with pkgs; [
-    openldap
-    exiftool
-  ];
+  #environment.systemPackages = with pkgs; [
+  #  openldap
+  #  exiftool
+  #];
 }
