11b7bad5620068e56b202966c23f867ce91266d5
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-16 11:47:34 +0100

    refactor graphical module

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -14,58 +14,60 @@
     xkbOptions = "caps:escape";
   };
   
-  services.xserver.displayManager.sessionCommands = let
-    myCustomXresources = pkgs.writeText "Xresources" ''
-      ! Copy selection to clipboard
-      XTerm*selectToClipboard: true
+  services.xserver.displayManager = {
+    sessionCommands = let
+      myCustomXresources = pkgs.writeText "Xresources" ''
+        ! Copy selection to clipboard
+        XTerm*selectToClipboard: true
 
-      ! powerline face for agnoster zsh theme
-      XTerm*faceName: Liberation Mono for Powerline
-      XTerm*faceSize: 11
+        ! powerline face for agnoster zsh theme
+        XTerm*faceName: Liberation Mono for Powerline
+        XTerm*faceSize: 11
 
-      ! Dark colours for xterm
-      XTerm*background: black
-      XTerm*foreground: lightgray
+        ! Dark colours for xterm
+        XTerm*background: black
+        XTerm*foreground: lightgray
 
-      ! Blinking cursor
-      XTerm*cursorBlink: true
+        ! Blinking cursor
+        XTerm*cursorBlink: true
 
-      ! XScreenSaver Custom Style
-      xscreensaver.splash: false
-      xscreensaver.Dialog.background: #000000
-      xscreensaver.Dialog.bottomShadowColor: #000000
-      xscreensaver.Dialog.foreground: #dddddd
-      xscreensaver.Dialog.topShadowColor: #000000
-      xscreensaver.Dialog.Button.background: #222222
-      xscreensaver.Dialog.Button.foreground: #eeeeee
-      xscreensaver.Dialog.text.background: #222222
-      xscreensaver.Dialog.text.foreground: #eeeeee
-      xscreensaver.Dialog.borderWidth: 0
-      xscreensaver.Dialog.internalBorderWidth: 24
-      xscreensaver.Dialog.shadowThickness: 2
-      xscreensaver.passwd.thermometer.background: #000000
-      xscreensaver.passwd.thermometer.foreground: #ff0000
-      xscreensaver.dateFormat: %Y-%m-%d %H:%M
+        ! XScreenSaver Custom Style
+        xscreensaver.splash: false
+        xscreensaver.Dialog.background: #000000
+        xscreensaver.Dialog.bottomShadowColor: #000000
+        xscreensaver.Dialog.foreground: #dddddd
+        xscreensaver.Dialog.topShadowColor: #000000
+        xscreensaver.Dialog.Button.background: #222222
+        xscreensaver.Dialog.Button.foreground: #eeeeee
+        xscreensaver.Dialog.text.background: #222222
+        xscreensaver.Dialog.text.foreground: #eeeeee
+        xscreensaver.Dialog.borderWidth: 0
+        xscreensaver.Dialog.internalBorderWidth: 24
+        xscreensaver.Dialog.shadowThickness: 2
+        xscreensaver.passwd.thermometer.background: #000000
+        xscreensaver.passwd.thermometer.foreground: #ff0000
+        xscreensaver.dateFormat: %Y-%m-%d %H:%M
+      '';
+      myCustomKeyboardLayout = pkgs.writeText "xkb-layout" ''
+        ! Map umlauts to ALT + <key>
+        keysym Alt_L = Mode_switch
+        keysym e = e E EuroSign
+        keysym a = a A adiaeresis Adiaeresis
+        keysym o = o O odiaeresis Odiaeresis
+        keysym u = u U udiaeresis Udiaeresis
+        keysym s = s S ssharp
+      '';
+    in ''
+      # merge Xresources:
+      ${pkgs.xorg.xrdb}/bin/xrdb -merge ${myCustomXresources}
+
+      # apply custom keyboard layout:
+      ${pkgs.xorg.xmodmap}/bin/xmodmap ${myCustomKeyboardLayout}
+      
+      # set background to solid black:
+      xsetroot -solid black
     '';
-    myCustomKeyboardLayout = pkgs.writeText "xkb-layout" ''
-      ! Map umlauts to ALT + <key>
-      keysym Alt_L = Mode_switch
-      keysym e = e E EuroSign
-      keysym a = a A adiaeresis Adiaeresis
-      keysym o = o O odiaeresis Odiaeresis
-      keysym u = u U udiaeresis Udiaeresis
-      keysym s = s S ssharp
-    '';
-  in ''
-    # merge Xresources:
-    ${pkgs.xorg.xrdb}/bin/xrdb -merge ${myCustomXresources}
-
-    # apply custom keyboard layout:
-    ${pkgs.xorg.xmodmap}/bin/xmodmap ${myCustomKeyboardLayout}
-    
-    # set background to solid black:
-    xsetroot -solid black
-  '';
+  };
 
   programs.qt5ct.enable = true;
 
