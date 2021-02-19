3bef04fa929a478abe32ee77f4244b3579bfbfc1
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-19 14:31:26 +0100

    add home-manager stub

--- modules/users.nix
+++ modules/users.nix
@@ -1,6 +1,19 @@
 { config, pkgs, ... }:
 
+# TODO: securely fetch home-manager (pick rev and ref)
+#let
+#  home-manager = builtins.fetchGit {
+#    url = "https://github.com/rycee/home-manager.git";
+#    rev = "55030c83024bf2d10ff86f3874b91794b9d32722";
+#    ref = "master";
+#  };
+#in
 {
+  #imports = [
+  #  #(import "${home-manager}/nixos")
+  #  (import "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos")
+  #];
+
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = true;
 
@@ -23,4 +36,6 @@
       uid = 1000;
     };
   };
+
+  #home-manager.users.savau = {};
 }
