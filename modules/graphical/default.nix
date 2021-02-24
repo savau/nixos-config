5c5bf3a52b1ff411d3669cf59978b4d5ba192291
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-24 17:42:01 +0100

    clear xkb options

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -14,6 +14,7 @@
     enable = true;
 
     layout = "us";
+    xkbOptions = "";
 
     displayManager.sessionCommands = let
       myXresources = pkgs.writeText "Xresources" (builtins.readFile ../../dotfiles/.Xresources);
