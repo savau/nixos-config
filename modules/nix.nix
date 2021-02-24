da0cd14c19a1eadf912bfa0070017c42707a1d87
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-24 11:36:17 +0100

    restrict allowedUsers to wheel group

--- modules/nix.nix
+++ modules/nix.nix
@@ -6,6 +6,7 @@
     buildCores = 0;         # use all available CPU cores when building
     daemonNiceLevel = 0;    # Nix daemon process priority (0: highest, 19: lowest)
     daemonIONiceLevel = 0;  # Nix daemon process I/O priority (0: highest, 7: lowest)
+    allowedUsers = [ "@wheel" ];  # grant the wheel user group permission to connect to the Nix daemon
     trustedUsers = [ "@wheel" ];  # grant the wheel user group additional rights when connecting to the Nix daemon
     binaryCaches = [ "https://cache.nixos.org/" ];  # list of binary cache URLs used to obtain pre-built binaries of Nix packages
     extraOptions = ''
