b8d2704b11ea363a56ff4c21ee7c560c8cf91a56
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-02-25 10:52:26 +0100

    tweak gtk config

--- modules/graphical/gtk.nix
+++ modules/graphical/gtk.nix
@@ -1,20 +1,31 @@
 { config, pkgs, ... }:
 
-let  
+let
+  myGtkExtraConfig = ''
+    gtk-enable-event-sounds=0
+    gtk-enable-input-feedback-sounds=0
+  '';
+
   myGtkConfig = {
     enable = true;
 
-    theme = null;
-    iconTheme = null;
-    font = null;
+    theme = {
+      name = "Arc-Dark";
+      package = pkgs.arc-theme;
+    };
+    iconTheme = {
+      name = "Arc";
+      package = pkgs.arc-icon-theme;
+    };
+    font = {
+      name = "Sans 10";
+    };
+
+    gtk2.extraConfig = myGtkExtraConfig;
+    gtk3.extraConfig = myGtkExtraConfig;
   };
 in
 {
-  environment.systemPackages = with pkgs; [
-    arc-theme
-    arc-icon-theme
-  ];
-
   home-manager.users = {
     root.gtk = myGtkConfig;
     savau.gtk = myGtkConfig;
