ce5902f0e0f48453d56fc06ff068f3f2becf0039
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-03-26 16:42:32 +0100

    Add flatpak, use sandboxed zoom

--- modules/security.nix
+++ modules/security.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  # https://flatpak.org/setup/NixOS/
+  # https://nixos.org/manual/nixos/stable/index.html#module-services-flatpak
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
+
   services.gnome3.gnome-keyring.enable = true;
-  
+
   environment.systemPackages = with pkgs; [
     keepassxc
   ];
--- modules/work/communication.nix
+++ modules/work/communication.nix
@@ -2,7 +2,16 @@
 
 {
   environment.systemPackages = with pkgs; [
-    zoom-us
     zulip
   ];
+
+  # Not adding zoom-us to systemPackages due to security concerns.
+  # Install a sandboxed (bubblewrapped) Zoom via flatpak instead:
+  #
+  # $ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  # $ flatpak update
+  # $ flatpak search zoom
+  # $ flatpak install flathub us.zoom.Zoom
+  #
+  # (See also: https://blog.laxu.de/2020/04/02/how-use-zoom-sandbox/)
 }
