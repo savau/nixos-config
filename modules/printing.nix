34418f841eb0b3cde822231e5979858fc71ee46b
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-04-03 20:58:48 +0200

    add hplip driver

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      hplip
+    ];
+  };
 
   environment.systemPackages = with pkgs; [
     simple-scan
