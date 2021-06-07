ad20472032a91b022b72b043c236f9da685096c1
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-07 14:50:12 +0200

    switch to home-manager (neo)vim config

--- modules/editor/vim/default.nix
+++ modules/editor/vim/default.nix
@@ -1,50 +1,29 @@
-args@{ pkgs, ... }:
+args@{ pkgs, lib, machine, ... }:
+
+with lib;
 
 let
-  rc = builtins.readFile ../../../dotfiles/.vimrc;
-  
-  # loaded on launch:
-  pluginsStart = with pkgs.vimPlugins; [
-    airline    # fancy bottom status line
-  # ale        # asynchronous lint engine
-    coc-nvim   # conquer of completion language server
-    coc-git    # git integration for coc
-    fugitive   # allows git operations within vim (via `:Git <command>`)
-    vim-nix    # Nix language support
-  ];
-  
-  # manually loadable via `:packadd $plugin-name`:
-  pluginsOpt = with pkgs.vimPlugins; [
-  ];
+  myVimPrograms.programs.neovim = {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      airline    # fancy bottom status line
+    # ale        # asynchronous lint engine
+      coc-nvim   # conquer of completion language server
+      coc-git    # git integration for coc
+      fugitive   # allows git operations within vim (via `:Git <command>`)
+      vim-nix    # Nix language support
+    ];
+    extraConfig = builtins.readFile ../../../dotfiles/.vimrc;
+  };
 in
 {
   imports = [
     (import ./coc args)
   ];
 
-  environment.systemPackages = with pkgs; [
-    nodejs  # coc requirement
-
-    (vim_configurable.customize {
-      name = "vim";
-      vimrcConfig = {
-        customRC = rc;
-        packages.myVimPackage = {
-          start = pluginsStart;
-          opt = pluginsOpt;
-        };
-      };
-    })
-    (neovim.override {
-      viAlias = true;
-      vimAlias = true;
-      configure = {
-        customRC = rc;
-        packages.myPlugins = {
-          start = pluginsStart;
-          opt = pluginsOpt;
-        };
-      };
-    })
-  ];
+  home-manager.users = (mapAttrs (_: _: myVimPrograms) machine.users) // { root = myVimPrograms; };
 }
