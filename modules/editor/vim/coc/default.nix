2672aa758d9928b42a0bdd56c7400f61375cc9dc
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-07 14:06:33 +0200

    move coc requirements to coc module

--- modules/editor/vim/coc/default.nix
+++ modules/editor/vim/coc/default.nix
@@ -1,8 +1,12 @@
-args:
+args@{ pkgs, ... }:
 
 {
   imports = [
   # choose ONE *.nix file in this directory
     (import ./basic.nix args)
+  ];
+
+  environment.systemPackages = with pkgs; [
+    nodejs  # coc requirement
   ];
 }
