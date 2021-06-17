469a66e615b511d62ce9a87310f300a344acefa7
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-17 15:53:43 +0200

    use neovim module from dotfiles in editor module

--- modules/editor/default.nix
+++ modules/editor/default.nix
@@ -1,12 +1,15 @@
-args@{ config, pkgs, ... }:
+args@{ config, pkgs, lib, machine, ... }:
 
+with lib;
+
+let
+  myEditors = import ../../dotfiles/vim/neovim args;
+in
 {
-  imports = [
-    (import ./vim args)
-  ];
-
   environment.variables = {
     EDITOR = "nvim";
     GIT_EDITOR = "nvim";
   };
+
+  home-manager.users = (mapAttrs (_: _: myEditors) machine.users) // { root = myEditors; };
 }
