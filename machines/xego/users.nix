2566fca95461e861d37b3decca85b5a1b989f28d
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-20 15:48:50 +0200

    machines/xego/users: refactor documentation; add TODOs

--- machines/xego/users.nix
+++ machines/xego/users.nix
@@ -1,35 +1,44 @@
+# Full user specification:
+#   <username> = {
+#     # account UID override
+#     # optional, defaults to next free UID
+#     id = 1000;
+# 
+#     # display name
+#     # optional, defaults to <username>
+#     displayName = "Default User";
+# 
+#     # user type to determine isNormalUser and isSystemUser
+#     # options: "normal" for humans, "system" for system users
+#     # optional, default: "normal"
+#     type = "normal";
+# 
+#     # should this user be a super user?
+#     isSuperUser = false;
+# 
+#     # user permissions
+#     permissions = {
+#       # should this user be granted all permissions available on this machine?
+#       all = false;  # TODO: replace with separate wheel flag
+# 
+#       # additional user permissions by group name
+#       # for a list of possible options, refer to /modules/users.nix
+#       additional = [];
+#     };
+# 
+#     # default shell
+#     shell = ./system-shell.nix;
+#   };
+#
+# Refactor user logic:
+# TODO: add global git config
+# TODO: add local-ish git config? (i.e. for u2w)
+# TODO: replace permissions.all with separate wheel/superuser flag
+# TODO: rename id -> uid
+# TODO: implement user group blacklist (i.e. to exclude a user from specific user groups)
+# TODO: replace additional permissions list with separate flags
+
 let
-  # Full user specification:
-  # The attribute name corresponds to the username
-  defaultUser = {
-    # user id
-    # TODO: make optional (use next free number if unspecified)
-    id = 1000;
-
-    # display name
-    displayName = "Default User";
-
-    # TODO: implement superuser flag
-
-    # user permissions
-    permissions = {
-      # should this user be granted all permissions available on this machine?
-      all = false;
-
-      # additional user permissions by group name
-      # possible options for this machine:
-      # [
-      #   "docker"
-      #   "libvirtd"
-      #   "systemd-journal"
-      #   "vboxusers"
-      # ];
-      additional = [];
-    };
-
-    # default shell
-    shell = ./system-shell.nix;
-  };
   pkgs = import <nixpkgs> {};
 in
 {
