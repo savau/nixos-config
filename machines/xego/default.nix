f98b581380afa02109ad40ed81a4d36248b10df6
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-20 16:39:45 +0200

    machine config: remove unnecessary comments, add TODOs

--- machines/xego/default.nix
+++ machines/xego/default.nix
@@ -1,40 +1,37 @@
 {
+  # TODO: remove, use directory name instead
   hostname = "xego";
 
   # UUID of the luks root partition
+  # TODO: add more customization, i.e. option to disable LUKS all together
   luksRootUUID = "d62c89b0-55b9-44a9-8d25-5fb5762ba422";
 
-  # Time zone this machine typically resides in
   timezone = "Europe/Berlin";
 
   # Machine users
+  # TODO: remove, use ./users.nix directly in config
   users = import ./users.nix;
 
   # System shell to use for all machine users including root. May be overriden per user via users.<user>.shell
+  # TODO: remove, use ./system-shell.nix directly in config
   systemShell = import ./system-shell.nix;
 
-  # Audio services on this machine
   audio = {
     enable = true;
   };
 
-  # Bluetooth services on this machine
   bluetooth = {
     enable = true;
   };
 
-  # Battery-management services on this machine
   batteryManagement = {
     enable = true;
   };
 
-  # SSD optimizations on this machine
-  # enable if NixOS is installed on an SSD drive
   ssdOptimizations = {
     enable = true;
   };
 
-  # Game services on this machine
   games = {
     enable = false;
   };
