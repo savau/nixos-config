7590ca607331b8060d6d54c21c6dc199c4a9e506
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-20 14:17:32 +0200

    add documentation stub for machine users

--- machines/xego/users.nix
+++ machines/xego/users.nix
@@ -1,4 +1,35 @@
 let
+  # Full user specification:
+  # The attribute name corresponds to the username
+  defaultUser = {
+    # user id
+    # TODO: make optional (use next free number if unspecified)
+    id = 1000;
+
+    # display name
+    displayName = "Default User";
+
+    # TODO: implement superuser flag
+
+    # user permissions
+    permissions = {
+      # should this user be granted all permissions available on this machine?
+      all = false;
+
+      # additional user permissions by group name
+      # possible options for this machine:
+      # [
+      #   "docker"
+      #   "libvirtd"
+      #   "systemd-journal"
+      #   "vboxusers"
+      # ];
+      additional = [];
+    };
+
+    # default shell
+    shell = ./system-shell.nix;
+  };
   pkgs = import <nixpkgs> {};
 in
 {
