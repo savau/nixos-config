271b25834f31e17b6f540714927926a4cb91990c
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-20 13:41:38 +0200

    add openmw to game module for debugging purposes

--- modules/entertainment/games.nix
+++ modules/entertainment/games.nix
@@ -7,6 +7,7 @@ then
 
   environment.systemPackages = with pkgs; [
     minetest
+    openmw  # FIXME: currently crashes with error: qt.qpa.plugin: Could not find the Qt platform plugin "xcb" in ""
   ];
 }
 else {}
