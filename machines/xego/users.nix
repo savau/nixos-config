36b63c0dbc0b998d12ccc7dab4af4c6ecdf34032
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-20 17:41:07 +0200

    machines/users: update/cleanup documentation

--- machines/xego/users.nix
+++ machines/xego/users.nix
@@ -1,43 +1,44 @@
-# Full user specification:
-# TODO: move documentation to /modules/users.nix
-#   <username> = {
-#     # account UID override
-#     # optional, default: next free UID
-#     uid = 1000;
-# 
-#     # display name
-#     # optional, default: <username>
-#     displayName = "Default User";
-# 
-#     # user type to determine isNormalUser and isSystemUser
-#     # options: "normal" for humans, "system" for system users
-#     # optional, default: "normal"
-#     type = "normal";
-# 
-#     # user permissions
-#     permissions = {
-#       # should this user be granted all available permissions?
-#       # optional, default: false
-#       all = false;
-# 
-#       # additional user permissions by group name
-#       # for a list of possible options, refer to /modules/users.nix
-#       groups = [];
-#
-#       # user permissions by group name to be excluded
-#       # the user will not be added to groups in this list; overrides permissions.all, permissions.groups and basic groups for normal users
-#       exclude = [];
-#     };
-# 
-#     # default shell
-#     shell = ./system-shell.nix;
-#   };
-#
-# Refactor user logic:
-# TODO: add global git config
-# TODO: add local-ish git config? (i.e. for u2w)
-
+# TODO: Refactor user logic:
+#   TODO: add global git config
+#   TODO: add local-ish git config? (i.e. for u2w)
 let
+  exampleUser = {
+    # account UID override
+    # optional, default: next free UID
+    uid = 1000;
+
+    # display name
+    # optional, default: <username>
+    displayName = "Default User";
+
+    # user type to determine isNormalUser and isSystemUser
+    # options: "normal" for humans, "system" for system users
+    # optional, default: "normal"
+    type = "normal";
+
+    # default shell
+    # optional, default: <system-shell>
+    shell = ./system-shell.nix;
+
+    # user permissions
+    # optional, default: {}
+    permissions = {
+      # should this user be granted all available permissions?
+      # optional, default: false
+      all = false;
+
+      # additional user permissions by group name
+      # for a list of possible options, refer to /modules/users.nix
+      # optional, default: []
+      groups = [];
+
+      # user permissions by group name to be excluded
+      # the user will not be added to groups in this list; overrides permissions.all, permissions.groups and basic groups for normal users
+      # optional, default: []
+      exclude = [];
+    };
+  };
+
   pkgs = import <nixpkgs> {};
 in
 {
