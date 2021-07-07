0d9ff7d13494a2a69db949008e6c21811d79f9e5
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-07-07 13:37:13 +0200

    refactor SSD optimizations module

--- modules/ssd-optimizations.nix
+++ modules/ssd-optimizations.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, machine, ... }:
 
 if machine.ssdOptimizations.enable then {
-  fileSystems."/".options = lib.optionals machine.ssdOptimizations.enable [ "noatime" "nodiratime" "discard" ];
-  services.fstrim.enable = machine.ssdOptimizations.enable;
+  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+  services.fstrim.enable = true;
 } else {}
