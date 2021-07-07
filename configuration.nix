91bb38f2c81b3dce5df3291ab410bd69b92bd62b
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-07-07 17:59:37 +0200

    module restructure: automatically import each module in modules/ directory instead of relying on manual registration via modules/default.nix

--- configuration.nix
+++ configuration.nix
@@ -7,11 +7,23 @@ let
   args = args' // { inherit machine; };
 in
 {
-  imports = [
-    # Include the results of the hardware scan.
-    (hostdir + "/hardware-configuration.nix")
-
-    # TODO: for each module in ./modules, import module with args instead
-    (import ./modules args)
-  ];
+  imports =
+    # include the results of the hardware scan
+    lib.singleton (hostdir + "/hardware-configuration.nix")
+    # import each module in ./modules
+    ++ (
+      lib.mapAttrsToList
+      (moduleName: moduleFileType:
+       if   moduleFileType == "regular" && builtins.match "[^ ]+.nix" moduleName == []
+         || moduleFileType == "directory"
+       then
+         import (./modules/. + "/${moduleName}") args
+       else
+         # if the module is neither a regular nix file nor a directory, it is either a regular non-nix file, a symlink or an unknown file type
+         # in this case, abort expression evaluation with error
+         # TODO: throw warning and ignore the module instead of aborting?
+         abort ("Unsupported module type: Found module " ++ moduleName ++ " of file type " ++ moduleFileType ++ ", but only regular *.nix files and directories are supported. Aborting.")
+      )
+      (builtins.readDir ./modules)
+    );
 }
