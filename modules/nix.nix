9359b0b46507fdea4e8a1f71d0fb24c2f7928471
Sarah Vaupel <sarah.vaupel@protonmail.com>
2022-02-02 15:34:32 +0100

    nix: refactor settings

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,13 +2,17 @@
 
 {
   nix = {
-    useSandbox = true;      # prevent impurities in builds by disallowing access to dependencies outside of the Nix store
-    buildCores = 0;         # use all available CPU cores when building
-  # daemonNiceLevel = 0;    # Nix daemon process priority (0: highest, 19: lowest)
-    daemonIOSchedPriority = 0;  # Nix daemon process I/O priority (0: highest, 7: lowest)
+  # daemonNiceLevel = 0;          # Nix daemon process priority (0: highest, 19: lowest)
+    daemonIOSchedPriority = 0;    # Nix daemon process I/O priority (0: highest, 7: lowest)
   # allowedUsers = [ "@wheel" ];  # grant the wheel user group permission to connect to the Nix daemon
-    trustedUsers = [ "@wheel" ];  # grant the wheel user group additional rights when connecting to the Nix daemon
-    binaryCaches = [ "https://cache.nixos.org/" ];  # list of binary cache URLs used to obtain pre-built binaries of Nix packages
+  
+    settings = {
+      sandbox = true;                                 # prevent impurities in builds by disallowing access to dependencies outside of the Nix store
+      cores = 0;                                      # use all available CPU cores when building
+      trusted-users = [ "@wheel" ];                   # grant the wheel user group additional rights when connecting to the Nix daemon
+      substituters = [ "https://cache.nixos.org/" ];  # list of binary cache URLs used to obtain pre-built binaries of Nix packages
+    };
+
     extraOptions = ''
       builders-use-substitutes = true
     '';  # enable the builder to use caches
