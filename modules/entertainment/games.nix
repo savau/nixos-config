cfe02829dbf393f5302c8a1aa09f0c8f2bba2a6e
Sarah Vaupel <sarah.vaupel@protonmail.com>
2022-05-13 10:41:40 +0200

    modules/entertainment: add wine

--- modules/entertainment/games.nix
+++ modules/entertainment/games.nix
@@ -4,6 +4,10 @@ if machine.games.enable then {
   programs.steam.enable = true;
 
   environment.systemPackages = with pkgs; [
+    wine
+    (wine.override { wineBuild = "wine64"; })
+    wineWowPackages.stable
+
     minetest
 #   openmw  # FIXME: currently crashes with error: qt.qpa.plugin: Could not find the Qt platform plugin "xcb" in ""
     wesnoth
