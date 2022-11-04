76841ce970026742c094a88a07d0e2e120a9b4dd
Sarah Vaupel <sarah.vaupel@protonmail.com>
2022-11-04 21:22:26 +0100

    update tmux

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,8 +1,25 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.tmux = {
     enable = true;
     clock24 = true;
+    historyLimit = 50000;
+    extraConfig = lib.readFile (pkgs.stdenv.mkDerivation {
+      name = "tmux.conf";
+      src = ./../dotfiles/tmux.conf;
+
+      buildInputs = with pkgs; [ makeWrapper ];
+
+      phases = [ "installPhase" ];
+
+      inherit (pkgs) zsh;
+      mandb = pkgs.man-db;
+
+      installPhase = ''
+        substituteAll $src $out
+      '';
+    });
+    # tmuxp.enable = true;
   };
 }
