d916edabd9678e0e9fc9c97a92a81d19010082e0
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-02-15 22:14:41 +0000

    machines/xego: update hardware-configuration.nix

--- machines/xego/hardware-configuration.nix
+++ machines/xego/hardware-configuration.nix
@@ -5,24 +5,35 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4c3f5372-461a-48a0-913e-c624c69235ca";
+    { device = "/dev/disk/by-uuid/24b0496f-2dcc-4bd0-8ca7-19ec56d733f3";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/B59E-EE44";
+    { device = "/dev/disk/by-uuid/1494-DA45";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/f68edaef-fa94-4b2a-a436-837abf8879d3"; }
+    [ { device = "/dev/disk/by-uuid/501f616b-3727-44f0-8047-4bd8577d7895"; }
     ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wwp0s20f0u7.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
