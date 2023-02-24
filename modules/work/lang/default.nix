945a3969e55d3452309838f917ee07d591c00587
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-02-24 23:23:04 +0100

    modules/work/lang: reenable js

--- modules/work/lang/default.nix
+++ modules/work/lang/default.nix
@@ -5,7 +5,7 @@ args:
   # (import ./agda.nix args)
     (import ./haskell.nix args)
   # (import ./java.nix args)
-  # (import ./javascript.nix args)
+    (import ./javascript.nix args)
   # (import ./octave.nix args)
     (import ./python.nix args)
   # (import ./r.nix args)
