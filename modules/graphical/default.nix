7123e6ad6918b930deb71952beca2016f3243eb4
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-02-24 22:47:03 +0100

    modules/graphical: remove megasync

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -85,34 +85,6 @@ in {
     birdtray
     evince
     nomacs
-    megasync
-    #OLDMEGASYNC.megasync
   ];
-
-  # https://github.com/NixOS/nixpkgs/issues/112572#issuecomment-780317989
-  #nixpkgs.overlays = [
-  #  (self: super:
-  #  let
-  #    version = "4.3.9.0";
-  #    srcFlavor = "Win";
-  #  in
-  #  {
-  #    megasync = super.megasync.overrideAttrs (
-  #      attrs: {
-  #        version = version;
-
-  #        src = super.fetchFromGitHub {
-  #          owner = "meganz";
-  #          repo = "MEGAsync";
-  #          rev = "v${version}_${srcFlavor}";
-  #          sha256 = "0lnm71hcda0lljfs12p8zw78d8a6xfb5xg5q9vxf2dsvgyniqq4p";
-  #          fetchSubmodules = true;
-  #        };
-
-  #        buildInputs = attrs.buildInputs ++ [ super.qt514.qtx11extras ];
-  #      }
-  #    );
-  #  })
-  #];
 
 }
