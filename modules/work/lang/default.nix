73f66a7ca84db6426710684342a6c60d6d590cba
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-02-24 22:51:24 +0100

    modules/work/lang: remove unneeded lang support

--- modules/work/lang/default.nix
+++ modules/work/lang/default.nix
@@ -2,14 +2,14 @@ args:
 
 {
   imports = [
-    (import ./agda.nix args)
+  # (import ./agda.nix args)
     (import ./haskell.nix args)
-    (import ./java.nix args)
-    (import ./javascript.nix args)
-    (import ./octave.nix args)
+  # (import ./java.nix args)
+  # (import ./javascript.nix args)
+  # (import ./octave.nix args)
     (import ./python.nix args)
-    (import ./r.nix args)
-    (import ./ruby.nix args)
+  # (import ./r.nix args)
+  # (import ./ruby.nix args)
     (import ./tex.nix args)
   ];
 }
