5794dc4ae5d63aa00a02728bb244a8e4f3aa2c8a
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-02-25 00:11:19 +0100

    modules: remove megasync debris

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -51,13 +51,7 @@ in {
 
   qt.platformTheme = "qt5ct";
 
-  environment.systemPackages =
-    let
-      # https://github.com/NixOS/nixpkgs/issues/112572#issuecomment-787204254
-      OLDMEGASYNC = import (pkgs.fetchzip {
-        url = "https://github.com/NixOS/nixpkgs/archive/4a7f99d55d299453a9c2397f90b33d1120669775.tar.gz";
-        sha256 = "14sdgw2am5k66im2vwb8139k5zxiywh3wy6bgfqbrqx2p4zlc3m7"; }) { config = { allowUnfree=true; }; };
-    in with pkgs; [
+  environment.systemPackages = with pkgs; [
     # accessibility stuff
     #at-spi2-atk at-spi2-core speechd
     #orca
