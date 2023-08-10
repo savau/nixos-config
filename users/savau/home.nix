2b0f20899d3a812f5d13df18f4e8c73c95444caf
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-10 23:10:47 +0200

    users/savau: fix home.packages syntax

--- users/savau/home.nix
+++ users/savau/home.nix
@@ -17,10 +17,10 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    pkgs.hello
+    hello
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
