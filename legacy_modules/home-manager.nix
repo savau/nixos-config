fe62d093d3adfc4d68e84ac434cc9be3fcec3005
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 02:29:16 +0200

    move old modules to legacy_modules (prepare for rewrite)

--- legacy_modules/home-manager.nix
+++ legacy_modules/home-manager.nix
@@ -1,21 +1,20 @@
 { config, pkgs, lib, machine, ... }:
 
-let
+# let
 # TODO: securely fetch home-manager (pick rev and ref)
 #  home-manager = builtins.fetchGit {
 #    url = "https://github.com/rycee/home-manager.git";
 #    rev = "55030c83024bf2d10ff86f3874b91794b9d32722";
 #    ref = "master";
 #  };
-  stateVersion = { home.stateVersion = "23.05"; };
-in
+# in
 {
   imports = [
     #(import "${home-manager}/nixos")
-    #(import "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos")
     <home-manager/nixos>
   ];
 
   system.stateVersion = "23.05";
-  home-manager.users = (lib.mapAttrs (_: _: stateVersion) machine.users) // { root = stateVersion; };
+
+  home-manager.users = lib.mapAttrs (_: _: stateVersion) machine.users;
 }
