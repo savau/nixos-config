c16e62e5dd6f3da14d935d6730dd5737950bf00b
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 20:55:32 +0200

    home-plugins/games: disable wine packages (currently broken)

--- home-plugins/games.nix
+++ home-plugins/games.nix
@@ -4,9 +4,9 @@
   home.packages = with pkgs; [
     steam
 
-    wine
-    (wine.override { wineBuild = "wine64"; })
-    wineWowPackages.stable
+    # wine
+    # (wine.override { wineBuild = "wine64"; })
+    # wineWowPackages.stable
 
     minetest
 #   openmw  # FIXME: currently crashes with error: qt.qpa.plugin: Could not find the Qt platform plugin "xcb" in ""
