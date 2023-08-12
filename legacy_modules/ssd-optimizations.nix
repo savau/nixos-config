13fe33b99a5489be95c569c1cb44d9b8661edec2
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 22:01:16 +0200

    remove obsolete ssd legacy module

--- legacy_modules/ssd-optimizations.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, lib, machine, ... }:
-
-if machine.ssdOptimizations.enable then {
-  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
-  services.fstrim.enable = true;
-} else {}
