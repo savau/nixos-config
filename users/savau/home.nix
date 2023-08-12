03828cd272818144b32e519a27f09e2c167babc4
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 21:15:27 +0200

    users/savau: temporarily disable xfce config files for debugging purposes

--- users/savau/home.nix
+++ users/savau/home.nix
@@ -100,10 +100,10 @@
   home.file = {
     ".Xmodmap".source = ./dotfiles/.Xmodmap;
 
-    ".config/xfce4" = {
-      source    = ./dotfiles/.config/xfce4;
-      recursive = true;
-    };
+    # ".config/xfce4" = {
+    #   source    = ./dotfiles/.config/xfce4;
+    #   recursive = true;
+    # };
 
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
