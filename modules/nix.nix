3261dbd2ffde6f9a7ac91eb2d0bd18e04963e63c
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 18:37:51 +0200

    modules/nix

---
+++ modules/nix.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  nix = {
+  # daemonNiceLevel = 0;          # Nix daemon process priority (0: highest, 19: lowest)
+    daemonIOSchedPriority = 0;    # Nix daemon process I/O priority (0: highest, 7: lowest)
+  # allowedUsers = [ "@wheel" ];  # grant the wheel user group permission to connect to the Nix daemon
+  
+    settings = {
+      sandbox = true;                                 # prevent impurities in builds by disallowing access to dependencies outside of the Nix store
+      cores = 0;                                      # use all available CPU cores when building
+      trusted-users = [ "@wheel" ];                   # grant the wheel user group additional rights when connecting to the Nix daemon
+      substituters = [ "https://cache.nixos.org/" ];  # list of binary cache URLs used to obtain pre-built binaries of Nix packages
+    };
+
+    extraOptions = ''
+      builders-use-substitutes = true
+      experimental-features = nix-command flakes
+    '';
+  };
+}
