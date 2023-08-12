52fc43e07e4565f853d231bfa0017ab33e72adeb
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 07:04:30 +0200

    users/savau: add .Xmodmap

--- users/savau/home.nix
+++ users/savau/home.nix
@@ -42,6 +42,8 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".Xmodmap".source = ./dotfiles/.Xmodmap;
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
