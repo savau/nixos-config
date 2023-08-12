c9fc27c3bd4adc8807d311ff1d6847a1d0350938
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 19:38:00 +0200

    users/savau: set editor session vars

--- users/savau/home.nix
+++ users/savau/home.nix
@@ -116,7 +116,8 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR     = "vim";
+    GIT_EDITOR = "vim";
   };
 
   programs = {
