4138fb3f67dbc43678b8e4d6838493f5db1ec5e6
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 05:08:32 +0200

    users/savau: remove redundant local xmonad config

--- users/savau/dotfiles/xmonad/lib/Utils/Dir.hs
+++
@@ -1,8 +0,0 @@
-module Utils.Dir
-  ( xMonad, statusBar, systemTray
-  ) where
-
-xMonad, statusBar, systemTray :: String
-xMonad     = "~/.xmonad/"
-statusBar  = xMonad <> "status-bar/"
-systemTray = xMonad <> "system-tray/"
