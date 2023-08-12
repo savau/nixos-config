ee667d4960b994f5bcd19d2b33eac85e921a3341
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 05:12:27 +0200

    users/savau: remove debug home package

--- users/savau/home.nix
+++ users/savau/home.nix
@@ -18,10 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
