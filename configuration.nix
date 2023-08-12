c8832e3eeff455f01bf4d58a7d4c4f34bdc66292
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-12 06:24:08 +0200

    configuration.nix: reintroduce modules to root config

--- configuration.nix
+++ configuration.nix
@@ -10,23 +10,20 @@ in
   imports = [
     # Include machine-specific configuration.nix
     ./hosts/${hostname}/configuration.nix
-  ];
-
+  ]
     # import each module in ./modules
-    # TODO: include modules in machine and user config
-    # ++ (
-    #   lib.mapAttrsToList
-    #   (moduleName: moduleFileType:
-    #    if   moduleFileType == "regular" && builtins.match "[^ ]+.nix" moduleName == []
-    #      || moduleFileType == "directory"
-    #    then
-    #      import (./modules/. + "/${moduleName}") args
-    #    else
-    #      # if the module is neither a regular nix file nor a directory, it is either a regular non-nix file, a symlink or an unknown file type
-    #      # in this case, abort expression evaluation with error
-    #      # TODO: throw warning and ignore the module instead of aborting?
-    #      abort ("Unsupported module type: Found module " ++ moduleName ++ " of file type " ++ moduleFileType ++ ", but only regular *.nix files and directories are supported. Aborting.")
-    #   )
-    #   (builtins.readDir ./modules)
-    # );
+    ++ (
+      lib.mapAttrsToList
+      (moduleName: moduleFileType:
+       if   moduleFileType == "regular" && builtins.match "[^ ]+.nix" moduleName == []
+         || moduleFileType == "directory"
+       then
+         import (./modules/. + "/${moduleName}") args
+       else
+         # if the module is neither a regular nix file nor a directory, it is either a regular non-nix file, a symlink or an unknown file type
+         # in this case, abort expression evaluation with error
+         abort ("Unsupported module type: Found module " ++ moduleName ++ " of file type " ++ moduleFileType ++ ", but only regular *.nix files and directories are supported. Aborting.")
+      )
+      (builtins.readDir ./modules)
+    );
 }
