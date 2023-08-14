6d70b7935f8d88d292e81cf74a0d4631aae807a6
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-14 02:05:12 +0200

    home-plugins/zsh: add nix-shell plugin

--- home-plugins/zsh.nix
+++ home-plugins/zsh.nix
@@ -27,6 +27,7 @@
           owner = "chisui";
           repo = "zsh-nix-shell";
           rev = "v0.7.0";
+          sha256 = "sha256-oQpYKBt0gmOSBgay2HgbXiDoZo5FoUKwyHSlUrOAP5E=";
         };
       }
     ];
