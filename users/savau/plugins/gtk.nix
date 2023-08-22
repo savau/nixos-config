133489e674d7c899c0331c5fe33601ec43f43f9f
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-22 19:44:07 +0200

    users/savau: fix gtk and dconf plugin

--- users/savau/plugins/gtk.nix
+++ users/savau/plugins/gtk.nix
@@ -2,16 +2,26 @@
 
 {
   home.packages = with pkgs; [
+    dconf
     gnome.adwaita-icon-theme
   ];
 
-  programs.dconf.enable = true;
+  dconf = {
+    enable = true;
+    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  };
 
   gtk = {
     enable = true;
 
-    theme.name = "Adwaita-Dark";
+    theme = {
+      name = "Adwaita-dark";
+    };
     iconTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+    };
+    cursorTheme = {
       name = "Adwaita";
       package = pkgs.gnome.adwaita-icon-theme;
     };
@@ -23,14 +33,17 @@
     gtk2.extraConfig = ''
       gtk-enable-event-sounds = 0
       gtk-enable-input-feedback-sounds = 0
+      gtk-application-prefer-dark-theme=1
     '';
     gtk3.extraConfig = {
       gtk-enable-event-sounds = 0;
       gtk-enable-input-feedback-sounds = 0;
+      gtk-application-prefer-dark-theme = 1;
     };
     gtk4.extraConfig = {
       gtk-enable-event-sounds = 0;
       gtk-enable-input-feedback-sounds = 0;
+      gtk-application-prefer-dark-theme = 1;
     };
   };
 }
