0328d0d2035387e39f89fd0352266bef7faf8da6
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-08-26 02:46:54 +0200

    users/savau: configure firefox plugin for privacy and security

--- users/savau/plugins/browsers/firefox.nix
+++ users/savau/plugins/browsers/firefox.nix
@@ -11,23 +11,36 @@ in
 
     profiles.default = {
       settings = {
+        "app.shield.optoutstudies.enabled" = false;
+        "extensions.formautofill.creditCards.enabled" = false;
+        "browser.bookmarks.restore_default_bookmarks" = false;
+        "browser.contentblocking.category" = "custom";
+        "browser.discovery.enabled" = false;
         "browser.download.autohideButton" = false;
         "browser.formfill.enable" = false;
         "browser.newtabpage.enabled" = false;
+        "browser.newtabpage.pinned" = "[]";
         "browser.privatebrowsing.autostart" = true;
         "browser.search.suggest.enabled.private" = true;
         "browser.startup.homepage" = "about:blank";
+        "browser.tabs.inTitlebar" = 0;
+        "browser.toolbars.bookmarks.visibility" = "never";
         "browser.topsites.contile.cachedTiles" = "[]";
-        "browser.uiCustomization.state" = "{\"placements\":{\"nav-bar\":[\"back-button\",\"forward-button\",\"urlbar-container\",\"bookmarks-menu-button\",\"downloads-button\",\"unified-extensions-button\"]}}";
+        "browser.topsites.contile.cacheValidFor" = 999999999999;
+        "browser.urlbar.placeholderName" = "DuckDuckGo";
+        "browser.urlbar.placeholderName.private" = "DuckDuckGo";
         "browser.urlbar.quicksuggest.engines" = false;
         "browser.urlbar.quicksuggest.history" = false;
         "browser.urlbar.quicksuggest.openpage" = false;
         "browser.urlbar.quicksuggest.topsites" = false;
         "datareporting.healthreport.uploadEnabled" = false;
         "dom.security.https_only_mode" = true;
+        "network.trr.mode" = 3;
+        "network.trr.uri" = "https://mozilla.cloudflare-dns.com/dns-query";
         "permissions.default.camera" = 2;
         "permissions.default.geo" = 2;
         "permissions.default.microphone" = 2;
+        "permissions.default.notifications" = 2;
         "permissions.default.xr" = 2;
         "places.history.enabled" = false;
         "privacy.clearOnShutdown.downloads" = true;
@@ -51,9 +64,7 @@ in
       };
 
       extensions = with nur.repos.rycee.firefox-addons; [
-        behind-the-overlay-revival
         darkreader
-        forget_me_not
         ghostery
         keepassxc-browser
         ublock-origin
