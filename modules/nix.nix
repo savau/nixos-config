1aa43eb9298e93a950eaeb89eaae4db4c064dd8c
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-09-01 22:38:05 +0200

    enable nix-command and flakes

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,11 +2,15 @@
 
 {
   nix = {
+    package = pkgs.nix;
+
+
   # daemonNiceLevel = 0;          # Nix daemon process priority (0: highest, 19: lowest)
     daemonIOSchedPriority = 0;    # Nix daemon process I/O priority (0: highest, 7: lowest)
   # allowedUsers = [ "@wheel" ];  # grant the wheel user group permission to connect to the Nix daemon
   
     settings = {
+      experimental-features = [ "nix-command" "flakes" ];
       sandbox = true;                                 # prevent impurities in builds by disallowing access to dependencies outside of the Nix store
       cores = 0;                                      # use all available CPU cores when building
       trusted-users = [ "@wheel" ];                   # grant the wheel user group additional rights when connecting to the Nix daemon
@@ -15,7 +19,6 @@
 
     extraOptions = ''
       builders-use-substitutes = true
-      experimental-features = nix-command flakes
     '';
   };
 }
--- users/savau/home.nix
+++ users/savau/home.nix
@@ -1,6 +1,11 @@
 { lib, pkgs, home, ... }:
 
 rec {
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
   imports = [
     # Host-agnostic generic home plugins
     ./../../home-plugins/direnv.nix
