5981b93faaa095361cef36045243a6fea7ff857b
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-09-03 19:02:42 +0200

    flake.nix: remove obsolete config options

--- flake.nix
+++ flake.nix
@@ -1,34 +1,10 @@
 {
   description = "savau's flakey NixOS configuration. Source: https://github.com/savau/nixos-config.git";
 
-  nixConfig = {
-    allowedUsers = [ "@wheel" ];  # grant the wheel user group permission to connect to the Nix daemon
-    daemonNiceLevel = 0;          # Nix daemon process priority (0: highest, 19: lowest)
-    daemonIOSchedPriority = 0;    # Nix daemon process I/O priority (0: highest, 7: lowest)
-
-    experimental-features = [ "nix-command" "flakes" ];
-    sandbox = true;                                 # prevent impurities in builds by disallowing access to dependencies outside of the Nix store
-    cores = 0;                                      # use all available CPU cores when building
-    trusted-users = [ "@wheel" ];                   # grant the wheel user group additional rights when connecting to the Nix daemon
-    substituters = [ "https://cache.nixos.org/" ];  # list of binary cache URLs used to obtain pre-built binaries of Nix packages
-
-    extraOptions = ''
-      builders-use-substitutes = true
-    '';
-  };
-
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
-    home-manager = {
-      url = github:nix-community/home-manager;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    xmonad-config = {
-      url = github:savau/xmonad-config;
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
+    home-manager.url = github:nix-community/home-manager;
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -38,7 +14,7 @@
         "tis"
         "gsateo"
       ];
-      mkDefaultSystem = host: nixpkgs.lib.nixosSystem {
+      mkHostConfiguration = host: nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = inputs;
         modules = [
@@ -46,6 +22,6 @@
           ./modules.nix
         ];
       };
-    in nixpkgs.lib.genAttrs hosts mkDefaultSystem;
+    in nixpkgs.lib.genAttrs hosts mkHostConfiguration;
   };
 }
