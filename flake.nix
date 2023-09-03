bd24994e9f51cb9ea2967359ca2ccf866a275544
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-09-03 16:13:24 +0200

    flake.nix: add nixConfig

--- flake.nix
+++ flake.nix
@@ -2,25 +2,20 @@
   description = "savau's flakey NixOS configuration. Source: https://github.com/savau/nixos-config.git";
 
   nixConfig = {
+    allowedUsers = [ "@wheel" ];  # grant the wheel user group permission to connect to the Nix daemon
+    daemonNiceLevel = 0;          # Nix daemon process priority (0: highest, 19: lowest)
+    daemonIOSchedPriority = 0;    # Nix daemon process I/O priority (0: highest, 7: lowest)
+
+    experimental-features = [ "nix-command" "flakes" ];
+    sandbox = true;                                 # prevent impurities in builds by disallowing access to dependencies outside of the Nix store
+    cores = 0;                                      # use all available CPU cores when building
+    trusted-users = [ "@wheel" ];                   # grant the wheel user group additional rights when connecting to the Nix daemon
+    substituters = [ "https://cache.nixos.org/" ];  # list of binary cache URLs used to obtain pre-built binaries of Nix packages
+
+    extraOptions = ''
+      builders-use-substitutes = true
+    '';
   };
-  # # package = pkgs.nix;
-
-  # # daemonNiceLevel = 0;          # Nix daemon process priority (0: highest, 19: lowest)
-  #   daemonIOSchedPriority = 0;    # Nix daemon process I/O priority (0: highest, 7: lowest)
-  # # allowedUsers = [ "@wheel" ];  # grant the wheel user group permission to connect to the Nix daemon
-  # 
-  #   settings = {
-  #     experimental-features = [ "nix-command" "flakes" ];
-  #     sandbox = true;                                 # prevent impurities in builds by disallowing access to dependencies outside of the Nix store
-  #     cores = 0;                                      # use all available CPU cores when building
-  #     trusted-users = [ "@wheel" ];                   # grant the wheel user group additional rights when connecting to the Nix daemon
-  #     substituters = [ "https://cache.nixos.org/" ];  # list of binary cache URLs used to obtain pre-built binaries of Nix packages
-  #   };
-
-  #   extraOptions = ''
-  #     builders-use-substitutes = true
-  #   '';
-  # };
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
