a50a98fb4dbf7ae41397650e1247556212ef7573
Sarah Vaupel <sarah.vaupel@protonmail.com>
2023-09-05 01:13:05 +0200

    hosts/xego: enable flakes for host

--- hosts/xego/configuration.nix
+++ hosts/xego/configuration.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, home-manager, ... }@args:
 
 {
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   imports = [
     # Include the results of the hardware scan
     ./hardware-configuration.nix
