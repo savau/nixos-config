11091d3090d40d842a00cd280c6775aa474a399c
Sarah Vaupel <sarah.vaupel@protonmail.com>
2024-01-15 19:44:12 +0100

    users/savau/zsh: update zsh-nix-shell

--- users/savau/plugins/zsh.nix
+++ users/savau/plugins/zsh.nix
@@ -14,7 +14,7 @@
       share = true;
     };
     shellAliases = {
-      "ll" = "ls -la";
+      "ll" = "ls -lh";
     };
     plugins = [
       {
@@ -23,8 +23,8 @@
         src = pkgs.fetchFromGitHub {
           owner = "chisui";
           repo = "zsh-nix-shell";
-          rev = "v0.7.0";
-          sha256 = "sha256-oQpYKBt0gmOSBgay2HgbXiDoZo5FoUKwyHSlUrOAP5E=";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
         };
       }
     ];
